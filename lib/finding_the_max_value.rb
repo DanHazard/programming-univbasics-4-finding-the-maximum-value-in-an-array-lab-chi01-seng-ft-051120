d3096f1bf0a853efbdcdc7acde55923718e08832
Dan Love <daniel.love.jr@gmail.com>
2020-04-28 21:30:25 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,7 +2,7 @@ def find_max_value(array)
   # Add your solution here
   max_num = 0 
   array.length.times do |index|
-    if array(index) > max_num
+    if array[index] > max_num
       max_num = array(index)
     end
   end
